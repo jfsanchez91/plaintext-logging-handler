15b118ce1d4ceefd5e60e87ce7254ffcfda126b2
Jorge F. Sánchez (George) <jorge.sanchez@syrocon.de>
2022-07-19 22:44:07 +0200

    Open DefaultLogStatementHandlerImpl to use config.

--- src/main/kotlin/net/jfsanchez/netty/handler/logging/DefaultLogStatementHandlerImpl.kt
+++ src/main/kotlin/net/jfsanchez/netty/handler/logging/DefaultLogStatementHandlerImpl.kt
@@ -4,12 +4,10 @@ import io.netty.channel.Channel
 import java.util.logging.Level
 import java.util.logging.Logger
 
-class DefaultLogStatementHandlerImpl : LogStatementHandler {
-    companion object {
-        private val logger: Logger = Logger.getLogger(DefaultLogStatementHandlerImpl::class.java.name)
-        private val logLevel: Level = Level.FINE
-    }
-
+class DefaultLogStatementHandlerImpl(
+    private val logger: Logger = Logger.getLogger(DefaultLogStatementHandlerImpl::class.java.name),
+    private val logLevel: Level = Level.FINE,
+) : LogStatementHandler {
     override fun handle(ctx: Channel, statement: String) {
         logger.log(logLevel, statement)
     }
